c8df1fd8a9399d564e79825209006b28221b3fd7
Peter O'Reilly <peter.o.reilly@ericsson.com>
2015-02-18 22:29:59 +0000

    adding DatabaseLocker.java - no implementation

---
+++ src/suncertify/db/DatabaseLocker.java
@@ -0,0 +1,22 @@
+package suncertify.db;
+
+public class DatabaseLocker {
+
+	public void lock(int recNo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean isLocked(int recNo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/**
+	 * @param recNo
+	 */
+	public void unlock(int recNo) {
+		// TODO Auto-generated method stub		
+	}
+
+}
